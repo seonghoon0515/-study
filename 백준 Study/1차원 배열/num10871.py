8f4111e708c36524b2a9ea5cdffb2f3bb648612a
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-11-07 16:17:30 +0900

    Add files via upload

---
+++ 백준 Study/1차원 배열/num10871.py
@@ -0,0 +1,8 @@
+# 백준 10871번 X보다 작은 수
+# 정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+N, X= map(int, input().split())
+list_A = list(map(int, input().split()))
+for i in range(len(list_A)) :
+    if list_A[i] < X :
+      print(list_A[i], end=" ")
+        
