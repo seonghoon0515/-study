63396a033cca2d9ee554e8099e7a7adefb487f2b
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-11-08 23:22:00 +0900

    Add files via upload

---
+++ 백준 Study/1차원 배열/num8958.py
@@ -0,0 +1,21 @@
+# 백준 8958번
+'''
+"OOXXOXXOOO"와 같은 OX퀴즈의 결과가 있다. O는 문제를 맞은 것이고, X는 문제를 틀린 것이다. 문제를 맞은 경우 그 문제의 점수는 그 문제까지 연속된 O의 개수가 된다. 예를 들어, 10번 문제의 점수는 3이 된다.
+
+"OOXXOXXOOO"의 점수는 1+2+0+0+1+0+0+1+2+3 = 10점이다.
+
+OX퀴즈의 결과가 주어졌을 때, 점수를 구하는 프로그램을 작성하시오.
+'''
+n = int(input())
+
+for _ in range(n):
+    ox_list = list(input())
+    score = 0  
+    sum_score = 0  # 새로운 ox리스트를 입력 받으면 점수 합계를 리셋한다.
+    for ox in ox_list:
+        if ox == 'O':
+            score += 1  # 'O'가 연속되면 점수가 1점씩 커진다.
+            sum_score += score  # sum_score = sum_score + score
+        else:
+            score = 0
+    print(sum_score)
