ffb695718607be83e25a41d8e881b16c9edcfd5c
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-11-17 22:47:54 +0900

    Update num15596.py

--- 백준 Study/함수/num15596.py
+++ 백준 Study/함수/num15596.py
@@ -1,8 +1,6 @@
-def sum(a) :
-    sum=0
-    for i in range(a) :
-        sum+=i
+# 백준 15596번 정수 n개가 주어졌을 때, n개의 합을 구하는 함수를 작성하시오.
+def solve(a) :
+    ans = sum(a)
 
-    return sum
+    return ans
 
-print(sum(5))
