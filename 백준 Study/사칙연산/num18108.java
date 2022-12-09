4a74fd9c6e90fd050365d53e0c08bcc17c0d2a63
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-12-09 20:04:47 +0900

    Add files via upload

---
+++ 백준 Study/사칙연산/num18108.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class num18108 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int bulgi = sc.nextInt();
+        int sugi = bulgi-543;
+        System.out.println(sugi);
+        sc.close();
+    }    
+}
