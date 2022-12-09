e2a1f51276baba90994eda47cb5e04ff70e44ff3
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-12-09 19:48:30 +0900

    Add files via upload

---
+++ 백준 Study/사칙연산/num10926.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class num10926 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String id = sc.nextLine();
+            System.out.println(id+"??!");
+        sc.close();
+    }
+}
