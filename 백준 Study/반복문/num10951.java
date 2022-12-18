ffa70160aa4a7c7acd292e9a8f425bcd4abf6ad1
Choi Seong Hoon <112906459+seonghoon0515@users.noreply.github.com>
2022-12-18 17:16:44 +0900

    Add files via upload

---
+++ 백준 Study/반복문/num10951.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class num10951 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            int n1 = sc.nextInt();
+            int n2 = sc.nextInt();
+            int sum=n1+n2;
+            System.out.println(sum);
+            if(false) break;
+        }
+        sc.close();
+    }
+}
